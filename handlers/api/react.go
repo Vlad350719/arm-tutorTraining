c3fe809106f18a2308fb164b63e2a183b360e7ae
vlad <iamvlad350719@gmail.com>
2025-05-23 23:54:16 +0200

    .

--- handlers/api/react.go
+++ handlers/api/react.go
@@ -7,7 +7,7 @@ import (
 
 func ReactHandler(w http.ResponseWriter, r *http.Request) {
 	data := map[string]string{
-		"message": "tendons and joints are the most important",
+		"message": "tendons and joints are the most important!",
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
