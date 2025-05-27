cf3085eb5b9d5003809eb5e326049197a727b480
vlad <iamvlad350719@gmail.com>
2025-05-27 22:49:07 +0200

    .

--- client/pages/about.jsx
+++ client/pages/about.jsx
@@ -2,8 +2,8 @@ export default function About() {
   return (
     <div>
       <h1>About</h1>
-      <p>Learn the art and science behind armwrestling techniques and training</p>
-<p>Learn how to become significantly stronger and smarter on the table</p>
+      <p>Learn the art and science behind armwrestling techniques and training!</p>
+<p>Learn how to become significantly stronger and smarter on the table!</p>
     </div>
   )
 }
